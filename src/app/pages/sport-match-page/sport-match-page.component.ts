ea399e8dc9dd67bb120e751f8cbc0c70d4a0510b
Zamir <zamir2211888@correo.uis.edu.co>
2025-05-01 01:59:02 -0500

    add carpeta partidos

--- src/app/pages/sport-match-page/sport-match-page.component.ts
+++ src/app/pages/sport-match-page/sport-match-page.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';  // Importante para los botones de mat-icon-button
+import { MatToolbarModule } from '@angular/material/toolbar';  // Para la barra de herramientas
 
 @Component({
   selector: 'app-sport-match-page',
-  imports: [],
+  imports: [MatTableModule,MatIconModule, MatButtonModule, MatToolbarModule],
   templateUrl: './sport-match-page.component.html',
   standalone: true,
   styleUrl: './sport-match-page.component.css',
 })
-export class SportMatchPageComponent {}
+export class SportMatchPageComponent {
+    // Datos de los partidos
+    partidos = [
+      { visitante: 'Equipo A', estadio: 'Estadio A', temporada: '2023', fecha: '2023-04-01' },
+      { visitante: 'Equipo B', estadio: 'Estadio B', temporada: '2023', fecha: '2023-04-02' },
+      { visitante: 'Equipo C', estadio: 'Estadio C', temporada: '2023', fecha: '2023-04-03' }
+    ];
+  
+    displayedColumns: string[] = ['visitante', 'estadio', 'temporada', 'fecha'];
+  }
