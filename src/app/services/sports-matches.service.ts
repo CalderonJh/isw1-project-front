56b63a1963e11fbe89a2e6fde963c2b06471afd9
Zamir <zamir2211888@correo.uis.edu.co>
2025-05-13 18:38:31 -0500

    Changes

--- src/app/services/sports-matches.service.ts
+++ src/app/services/sports-matches.service.ts
@@ -91,8 +91,4 @@ export class SportsMatchesService {
     });
     return this.http.delete<void>(`${this.apiUrl}/delete/${id}`, { headers });
   }
-<<<<<<< HEAD
 }
-=======
-} 
->>>>>>> origin/develop_Cristian
